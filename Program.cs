666bd0cd18b1feae3a3f772017e32f9762dbece7
subaa1492 <60738126+subaa1492@users.noreply.github.com>
2021-05-11 18:39:39 +0530

    how to work with appointments in blazor scheduler

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+
+namespace Scheduler
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDM0MzAyQDMxMzkyZTMxMmUzMGVkQzliU1RSd3ptZDRQeHV5K0N3UjJnemNvNWhCRGJEZ1NUVUJHOGc0blk9");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
